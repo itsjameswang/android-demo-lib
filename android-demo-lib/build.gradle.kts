3a6ad065e7fac4dc343573e1f421ddacc92bcee0
James Wang <domo90a325@gmail.com>
2024-02-26 15:55:09 +0800

    commit2

--- android-demo-lib/build.gradle.kts
+++ android-demo-lib/build.gradle.kts
@@ -1,6 +1,42 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            // 创建 Maven 发布配置
+            create<MavenPublication>("mavenJava") {
+                // 指定要发布的组件
+                from(components["release"])
+
+                // 添加库的元数据
+                groupId = "com.github.itsjameswang"
+                artifactId = "android-demo-lib"
+                version = "1.1"
+            }
+        }
+        repositories {               // << --- ADD This
+            mavenLocal()
+        }
+
+        // 配置 Maven 存储库
+//        repositories {
+//            maven {
+//
+//                // 指定 Maven 仓库 URL，下面以 GitHub Packages 为例
+//                url = uri("https://maven.pkg.github.com/itsjameswang/LibraryTesting")
+//
+//                // 配置访问凭证
+//                credentials {
+//                    username = "itsjameswang" // GitHub 用户名或环境变量
+//                    password = "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"   // GitHub 令牌或环境变量
+//                }
+//            }
+//        }
+    }
 }
 
 android {
@@ -41,4 +77,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.android.volley:volley:1.2.1")
 }
